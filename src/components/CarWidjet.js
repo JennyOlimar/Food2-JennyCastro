90bae047b96ecb51e8fa05326ab5164c61ab7c03
Jenny <jcastro192774@gmail.com>
2022-06-05 13:43:08 -0300

    Cart View

--- src/components/CarWidjet.js
+++ src/components/CarWidjet.js
@@ -1,13 +1,25 @@
+import { AppContext } from "../app/CartContext";
+import { useContext } from "react";
 import imagenes from "../assets/imagenes";
+import { Link } from "react-router-dom";
 
 const CarWidjet = () => {
+    const [items, , , , ] = useContext(AppContext);
+    const itemVarios = items.map(item => item);
+    const itemUnico = itemVarios.map(item => item.cantidad);
+    const totalItems = itemUnico.length > 0 ? itemUnico.reduce((result,number)=> result+number) : [];
     return<>
-        <div className="flexAlignCenter">
-            <a className="ref" href="./ItemListContainer.js">
-                <img className="imgCar" src={imagenes.car} alt="" />
-                <span>Mi carrito</span>
-            </a>
-        </div>
+        {
+            items.length > 0 &&
+            <div className="flexAlignCenter">
+                <Link className="ref" to="/cart">
+                    <span className="textCar">Mi carrito</span>
+                    <img className="imgCar" src={imagenes.car} alt="" />
+                    <span className="badge">{totalItems}</span>
+                </Link>
+            </div>
+        }
+        
     </>
 }
 
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,16 +1,17 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../app/CartContext";
 import { createItem } from "../app/orders-firebase";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const [items, , , , ] = useContext(AppContext);
+  const [items, , removeItem, , ] = useContext(AppContext);
   const [name, setName] = useState('');
   const [phone, setPhone] = useState(0);
   const [email, setEmail] = useState('');
 
   const producto = items.map(item => ({ ...item, id: item.id, title: item.title, price: item.price}));
   const arr = items.map(item => item.cantidad * item.price);
-  const totalProductos = arr.reduce((result,number)=> result+number);
+  const totalProductos = arr.length > 0 ? arr.reduce((result,number)=> result+number) : [];
 
   const ordenCompra = {
     buyer: {
@@ -29,15 +30,30 @@ const Cart = () => {
     <input className="form-control margin10" type="text" placeholder="Nombre" onChange={(e) => setName(e.target.value)}/>
     <input className="form-control margin10" type="tel" placeholder="Teléfono" onChange={(e) => setPhone(e.target.value)}/>
     <input className="form-control margin10" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-    <h6 className="cardText">Productos para la compra:</h6>
-    {items.map(item => 
-      <div className="margin10" key={item.id}>
-        <p>{item.title}</p>
-        <p>{item.cantidad}</p>
-        <hr />
+    {
+      arr.length > 0 ?
+      <div>
+        <h6 className="cardText">Productos para la compra:</h6>
+        {items.map(item => 
+          <div className="margin10 item" key={item.id}>
+            <p>Nombre: {item.title}</p>
+            <p>Cantidad: {item.cantidad}</p>
+            <p>Precio: {item.price}</p>
+            <button className="btn btn-secondary" onClick={() => removeItem(item.id)}>Borrar item</button>
+          </div>
+          )}
+        <p className="total">Costo total: {totalProductos}</p>
+        <div className="space">
+          <button className="btn btn-info"><Link to='/'>Agregar Productos</Link></button>
+          <button className="btn btn-info" onClick={() => createItem(ordenCompra)}>Comprar</button>
+        </div>
+      </div> :
+      <div className="addItems">
+        <p className="alert alert-danger">No hay productos en el carrito</p>
+        <button className="btn btn-info"><Link to='/'>Agregar Productos</Link></button>
       </div>
-      )}
-    <button className="btn btn-info cardButton" onClick={() => createItem(ordenCompra)}>Comprar</button>
+    }
+    
     </div>
   )
 }
